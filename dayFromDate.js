def7644af466713710705e03548754a400f70591
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-03 02:52:23 +0600

    dayName from date

--- dayFromDate.js
+++ dayFromDate.js
@@ -1,9 +1,11 @@
 // The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 function getDayName(dateString) {
-    let dayName;
-    // Write your code here
-    dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][new Date(`${dateString}`).getDay()];
+    let dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][new Date(dateString).getDay()];
     return dayName;
 }
 let result = getDayName(10/12/2022);
 console.log(result);
+
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var dayName = days[new Date(10/12/2022).getDay()];
+console.log(dayName)
