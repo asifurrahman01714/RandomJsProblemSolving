7970f4a719c741dea1e63f9a0dedd9953e78cf8d
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-07 03:06:39 +0600

    const + object

---
+++ const.js
@@ -0,0 +1,10 @@
+// Create an object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// Change a property:
+car.color = "red";
+
+// Add a property:
+car.owner = "Johnson";
+
+console.log(car);
