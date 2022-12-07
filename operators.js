c73b82d2d5f31fedbf402d6536f6abf2d7eaa608
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-07 14:59:23 +0600

    various operators in js

---
+++ operators.js
@@ -0,0 +1,11 @@
+/*
+Types of JavaScript Operators
+There are different types of JavaScript operators:
+
+    Arithmetic Operators
+    Assignment Operators
+    Comparison Operators
+    Logical Operators
+    Conditional Operators
+    Type Operators
+*/
