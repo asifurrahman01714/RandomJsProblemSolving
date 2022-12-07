862c35a8af23299101eb9a75e878c9da24ef4bb4
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-07 18:49:25 +0600

    function parameters and arguments

--- jsFunction.js
+++ jsFunction.js
@@ -1,3 +1,7 @@
+/* A JavaScript function is a block of code designed to perform a particular task.
+A JavaScript function is executed when "something" invokes it (calls it).
+*/
+
 {
     function myFunction(p1, p2) {
         return p1 * p2;
@@ -5,3 +9,8 @@
     const result = myFunction(2,3)
     console.log(result);
 }
+
+/*
+Function parameters are listed inside the parentheses () in the function definition.
+Function arguments are the values received by the function when it is invoked.
+*/
