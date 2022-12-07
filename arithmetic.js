b86f78bc67d346cb8b59cd3193cd986731885117
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-07 15:27:44 +0600

    exponentiation operator (**)

--- arithmetic.js
+++ arithmetic.js
@@ -1,3 +1,9 @@
 let x = 5;
 let z = x ** 2; //The exponentiation operator (**) raises the first operand to the power of the second operand.
 console.log(z);
+
+{
+    let x = 5;
+    let z = Math.pow(x,2); // x ** y produces the same result as Math.pow(x,y):
+    console.log(z)
+}
