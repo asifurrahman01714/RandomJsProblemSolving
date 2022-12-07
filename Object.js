71ac90237834ed9c98086fd4d7fdcbc150d2c76e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-07 23:04:41 +0600

    object

--- Object.js
+++ Object.js
@@ -5,3 +5,16 @@ objectName.propertyName
 or
 objectName["propertyName"]
 */
+
+{
+    // Create an object:
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id     :  5566
+  };
+  
+  // Display some data from the object:
+    console.log(person.firstName + " " + person.lastName);
+    console.log(person["firstName"] + " " + person["lastName"]);
+}
