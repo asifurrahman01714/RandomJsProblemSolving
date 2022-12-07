73da9acd02e89458a125a7579bed8cf02b428fff
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-07 23:02:13 +0600

    object.js

---
+++ Object.js
@@ -0,0 +1,7 @@
+/*
+Accessing Object Properties
+You can access object properties in two ways:
+objectName.propertyName
+or
+objectName["propertyName"]
+*/
--- jsFunction.js
+++ jsFunction.js
@@ -15,3 +15,38 @@ Function parameters are listed inside the parentheses () in the function definit
 Function arguments are the values received by the function when it is invoked.
 Inside the function, the arguments (the parameters) behave as local variables.
 */
+
+/*
+
+Function Invocation
+The code inside the function will execute when "something" invokes (calls) the function:
+
+When an event occurs (when a user clicks a button)
+When it is invoked (called) from JavaScript code
+Automatically (self invoked)
+You will learn a lot more about function invocation later in this tutorial.
+
+*/
+
+/*
+Function Return
+When JavaScript reaches a return statement, the function will stop executing.
+If the function was invoked from a statement, JavaScript will "return" to execute the code after the invoking statement.
+Functions often compute a return value. The return value is "returned" back to the "caller":
+
+*/
+
+/*
+Why Functions?
+You can reuse code: Define the code once, and use it many times.
+You can use the same code many times with different arguments, to produce different results.
+*/
+
+
+{
+    function toCelsius(f) {
+        return (5/9) * (f-32);
+      }
+    const result = toCelsius;
+    console.log(result) // this will return the toCelsius function
+}
