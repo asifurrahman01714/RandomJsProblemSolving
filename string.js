a6af96605db1341d0d30e4c4b1e6a12a9d50bc1a
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-08 10:21:05 +0600

    slice and substring work same

--- string.js
+++ string.js
@@ -2,4 +2,9 @@
     let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let length = text.length;
     console.log(length) // string length is not a function . it is a property of string.
+
+    // Extracting String Parts
+    console.log(text.slice(1,2));
+    console.log(text.substring(1,2));
+    console.log(text.substr(10,length));
 }
