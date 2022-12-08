5d780b920312d5cec40819b59f9c7ef5e81e033d
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-08 10:35:41 +0600

    substr

--- string.js
+++ string.js
@@ -8,3 +8,16 @@
     console.log(text.substring(1,2));
     console.log(text.substr(10,length));
 }
+{
+    let text = "Apple, Banana, Kiwi";
+    let part = text.slice(-12);
+    console.log(part)
+    console.log(text.slice(12))
+    console.log(text.slice(-12, -6))
+    console.log(text[15]);
+}
+{
+    let str = "Apple, Banana, Kiwi";
+    console.log(str.substr(7, 6))
+    console.log(str.substr(-4))
+}
