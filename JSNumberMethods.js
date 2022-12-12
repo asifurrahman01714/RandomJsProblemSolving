6ebfcc15f3cd71582f80bb555d22df579561a15e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-12 22:15:27 +0600

    toPrecision

--- JSNumberMethods.js
+++ JSNumberMethods.js
@@ -15,4 +15,10 @@
     console.log(toFixed)
     console.log(typeof toFixed)
     console.log(toFixed) // toFixed returns a string
+
+    console.log(x.toPrecision());
+    console.log(x.toPrecision(2))
+    console.log(x.toPrecision(3))
+    console.log(x.toPrecision(4))
+    console.log(x.toPrecision(5))
 }
