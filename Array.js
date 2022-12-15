4bb979116a3fca3f373f31683f075c1f904bbff8
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-15 21:24:14 +0600

    Array.js

--- Array.js
+++ Array.js
@@ -7,3 +7,10 @@
     cars[2]= "BMW";
     console.log(cars)
 }
+
+{
+    // using new keyword
+    const cars = new Array("Saab", "Volvo", "BMW");
+    console.log(cars);
+    console.log(typeof cars);
+}
