8a189fafa93f8de62783398af585bf446aee1959
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-15 21:22:18 +0600

    Array.js

---
+++ Array.js
@@ -0,0 +1,9 @@
+{
+    // create an array, and then provide the elements:
+    const cars = [];
+    console.log(cars);
+    cars[0]= "Saab";
+    cars[1]= "Volvo";
+    cars[2]= "BMW";
+    console.log(cars)
+}
