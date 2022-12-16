153c3eedae0854089b252d5789d6612e1f44d466
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-17 04:49:05 +0600

    Array loop in forEach

--- Array.js
+++ Array.js
@@ -42,3 +42,16 @@
     text += "</ul>";
     console.log(text);
 }
+{
+    // Array loop in forEach
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+    let text = "<ul>";
+    fruits.forEach(myFunction);
+    text += "</ul>";
+
+    function myFunction(value) {
+    text += "<li>" + value + "</li>";
+    }
+    console.log(text)
+}
