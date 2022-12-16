80fa1d5b754a889636ffcf2171ce13db5a7add2a
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-12-17 04:44:05 +0600

    You can have objects in an Array. You can have functions in an Array. You can have arrays in an Array:

--- Array.js
+++ Array.js
@@ -14,3 +14,19 @@
     console.log(cars);
     console.log(typeof cars);
 }
+{
+    // You can have objects in an Array. You can have functions in an Array. You can have arrays in an Array:
+    const mainArray = [];
+    
+    mainArray[0] = mainFunction();
+    mainArray[1] = {name: "Asif", Id: 12};
+    mainArray[2] = ["asif", "sakib"];
+    function mainFunction() {
+        return "hello world";
+    }
+    console.log(mainArray)
+    console.log(mainArray[0])
+    console.log(mainArray[1])
+    console.log(mainArray[1].name)
+    console.log(mainArray[2][0])
+}
